67c6debcd0d5944a322ec6368264d988e74468d8
Lambert Green <lambert_green@msn.com>
2023-05-05 12:33:56 -0700

    Adds init.lua

---
+++ init.lua
@@ -0,0 +1,15 @@
+-------------------------------------------------
+-- Lambert's Neovim configuration
+-------------------------------------------------
+
+local g = vim.g
+local o = vim.o
+local opt = vim.opt
+
+o.termguicolors = true
+
+opt.mouse = "a"
+
+-- Map <leader> to space
+g.mapleader = " "
+g.maplocalleader = " "
