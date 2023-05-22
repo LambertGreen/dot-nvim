3af5d91764ebf380aae6eb2c2a1faef0328f82c3
Lambert Green <lambert_green@msn.com>
2023-05-22 10:34:38 -0700

    Updates formatting options

--- init.lua
+++ init.lua
@@ -7,23 +7,27 @@
 -------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -------------------------------------------------
 -- Vim Settings
 -------------------------------------------------
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 -- Enable mouse support in the terminal
 vim.opt.mouse = "a"
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
 -- Map <leader> to space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
