fe5f80adb0cc15bdd01f34de209af064d41e89c1
Lambert Green <lambert_green@msn.com>
2023-06-02 12:24:54 -0700

    Adds keymappings
    Referenced from my old Vim config.

--- init.lua
+++ init.lua
@@ -52,10 +52,36 @@ vim.opt.guifont  = "Iosevka NF:h11"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Use useful Emacs/Readline bindings while in Insert mode
+vim.keymap.set('i', '<C-a>', '<C-o>0', { })
+vim.keymap.set('i', '<C-e>', '<C-o>$', { })
+
+-- Buffer management
 vim.keymap.set('n', '<leader>bs', ':w<CR>', { silent = true, desc = "Buffer save" })
 vim.keymap.set('n', '<leader>bd', ':bd<CR>', { silent = true, desc = "Buffer delete" })
-vim.keymap.set('n', '<leader>qq', ':q<CR>', { silent = true, desc = "Quit Vim" })
+vim.keymap.set('n', '<leader>bp', ':bp<CR>', { silent = true, desc = "Buffer previous" })
+vim.keymap.set('n', '<leader>bn', ':bn<CR>', { silent = true, desc = "Buffer next" })
+
+-- Window management
+vim.keymap.set('n', '<leader>ws', ':split<CR>', { silent = true, desc = "Window split" })
+vim.keymap.set('n', '<leader>wv', ':vsplit<CR>', { silent = true, desc = "Window vertical split" })
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { silent = true, desc = "Move to left window" })
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { silent = true, desc = "Move to left window" })
+vim.keymap.set('n', '<leader>wj', '<C-w>j', { silent = true, desc = "Move to bottom window" })
+vim.keymap.set('n', '<leader>wk', '<C-w>k', { silent = true, desc = "Move to top window" })
+vim.keymap.set('n', '<leader>wl', '<C-w>l', { silent = true, desc = "Move to right window" })
+vim.keymap.set('n', '<leader>ww', '<C-w>w', { silent = true, desc = "Move to next window" })
+vim.keymap.set('n', '<leader>wq', ':q<CR>', { silent = true, desc = "Move to next window" })
+
+-- Terminal
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { silent = true, desc = "Move to next window" })
+
+-- Quitting
+vim.keymap.set('n', '<leader>qq', ':qall<CR>', { silent = true, desc = "Quit Vim" })
+
+-- Code: remove trailing whitespace
 vim.keymap.set('n', '<leader>cw', ':%s/\\s\\+$//e<CR>', { noremap = true, silent = true })
+
 
 ------------------------------------------------
 -- Setup packages
