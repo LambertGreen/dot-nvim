e0e03ef7975194a0600cce2c2ec1a48018ecefb2
Lambert Green <lambert_green@msn.com>
2023-12-04 19:59:46 -0800

    Move function into functions.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
 require("lgreen.core")
 require("lgreen.lazy")
 
--- Code: time command
-function _G.time_command(cmd)
-  local start_time = vim.loop.hrtime()
-  vim.cmd(cmd)
-  local elapsed_time = vim.loop.hrtime() - start_time
-  print("Command " .. cmd .. " took " .. elapsed_time / 1e9 .. " seconds")
-end
---
+++ lua/lgreen/core/functions.lua
@@ -0,0 +1,8 @@
+
+-- Code: time command
+function _G.time_command(cmd)
+  local start_time = vim.loop.hrtime()
+  vim.cmd(cmd)
+  local elapsed_time = vim.loop.hrtime() - start_time
+  print("Command " .. cmd .. " took " .. elapsed_time / 1e9 .. " seconds")
+end
