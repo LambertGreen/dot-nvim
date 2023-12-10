45588d21d9af0aba5f50d7f962d75fbb5bc67eed
Lambert Green <lambert_green@msn.com>
2023-12-09 22:10:49 -0800

    Increase the size of toggleterm

--- lua/lgreen/plugins/toggleterm.lua
+++ lua/lgreen/plugins/toggleterm.lua
@@ -1,9 +1,13 @@
 return
-{
-  'akinsho/toggleterm.nvim',
-  version = "*",
-  config = true,
-  keys = {
-    { '<leader>ot', '<cmd>ToggleTerm<CR>', desc = 'ToggleTerm' }
-  },
-}
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup{
+        size = 30
+      }
+    end,
+    keys = {
+      { '<leader>ot', '<cmd>ToggleTerm<CR>', desc = 'ToggleTerm' }
+    },
+  }
